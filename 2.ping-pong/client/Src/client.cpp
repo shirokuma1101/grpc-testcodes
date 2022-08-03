9aee55da900a7cde2f1fcf48a6d3a53896e25000
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-08-03 17:36:21 +0900

    add: ping-pong版ソースを追加

---
+++ 2.ping-pong/client/Src/client.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+#include "Proto/ping-pong.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using ::PingPong;
+using ::Ping;
+using ::Pong;
+
+class Client {
+public:
+    Client(std::shared_ptr<Channel> channel)
+        : stub(PingPong::NewStub(channel))
+    {}
+
+    UINT64 SendPing(UINT64 real_time) {
+        ClientContext context;
+        Ping ping;
+        ping.set_time(real_time);
+        Pong pong;
+
+        Status status = stub->SendPing(&context, ping, &pong);
+
+        if (status.ok()) {
+            return pong.time();
+        }
+        else {
+            std::cout << status.error_code() << ":" << status.error_message() << std::endl;
+            return 0;
+        }
+    }
+
+private:
+
+    std::unique_ptr<PingPong::Stub> stub;
+
+};
+
+int main() {
+    UINT64 rt = common::GetRealTime();
+    std::string server_addr = "localhost:8080";
+    Client client(grpc::CreateChannel(server_addr, grpc::InsecureChannelCredentials()));
+
+    UINT64 response = client.SendPing(rt);
+    UINT64 latency = response - rt;
+    auto st = common::GetSystemTime(latency);
+    std::cout << "Latency:" << st.wMilliseconds << "ms" << std::endl;
+
+    return 0;
+}
