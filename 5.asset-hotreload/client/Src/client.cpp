5d6833b09cdcfb2cf2f5c78401608179332158e1
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-08-25 17:34:15 +0900

    add: assetdata/assetmanagerを利用したhotreload用ソースを追加

---
+++ 5.asset-hotreload/client/Src/client.cpp
@@ -0,0 +1,22 @@
+﻿#include "client.h"
+
+int main() {
+    // コマンドライン引数
+    auto vm = common::GetCmdLine();
+
+    auto& host = vm["host"].as<std::string>();
+    auto& port = vm["port"].as<std::string>();
+
+    std::string server_addr = host + ":" + port;
+    auto client = std::make_shared<Client>(server_addr);
+
+    auto file_path = client->GetFilePath(AssetType::JSON, "test");
+    
+    // save the file to file_path
+    std::cout << file_path << std::endl;
+
+    //TODO: async
+    client->HotReload(AssetType::JSON, "test");
+
+    return 0;
+}
---
+++ 5.asset-hotreload/client/Src/client.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include "Proto/asset-hotreload.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using ::HotReloadService;
+using ::Request;
+using ::FilePath;
+using ::Successed;
+using ::AssetType;
+
+class Client {
+public:
+
+    Client(const std::string& host)
+        : m_upStub(HotReloadService::NewStub(grpc::CreateChannel(host, grpc::InsecureChannelCredentials())))
+    {}
+
+    std::string GetFilePath(AssetType asset_type, std::string_view asset_name) {
+        ClientContext ctx;
+        Request request;
+        request.set_name(asset_name.data());
+        request.set_asset_type(asset_type);
+        FilePath file_path;
+        CheckStatus(m_upStub->GetFilePath(&ctx, request, &file_path));
+        return file_path.file_path();
+    }
+
+    bool HotReload(AssetType asset_type, std::string_view asset_name) {
+        ClientContext ctx;
+        Request request;
+        request.set_asset_type(asset_type);
+        request.set_name(asset_name.data());
+        Successed successed;
+        CheckStatus(m_upStub->HotReload(&ctx, request, &successed));
+        return successed.successed();
+    }
+
+private:
+
+    bool CheckStatus(const Status& status, std::string detail = {}) {
+        if (status.ok()) return true;
+        else {
+            detail = status.error_code() + ":" + status.error_message();
+            assert(0);
+            return false;
+        }
+    }
+
+    std::unique_ptr<HotReloadService::Stub> m_upStub;
+
+};
